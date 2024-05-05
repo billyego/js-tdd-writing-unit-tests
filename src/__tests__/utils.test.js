93210dac94a2b578184677961d0e9859c147b67d
Bill <bill.kimtai@student.moringaschool.com>
2024-05-05 15:47:20 +0300

    complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,9 @@
-// Your tests here
+import {pointsForWord} from "../utils"
+
+describe("PointsForWord",()=>{
+    it("calculates the total points for a word (1 point per vowel,2 per constant)",()=>{
+        const word="test";
+        const points = pointsForWord(word);
+        expect(points).toBe(7);
+    })
+})
